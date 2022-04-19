488cadcfc56da3568b690614db166b913d1e6244
Manish <manishboge@gmail.com>
2022-04-19 22:30:40 +0530

    feat: migrated to scss from css

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -6,7 +6,7 @@ import { LocationService } from '../location.service';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
 
